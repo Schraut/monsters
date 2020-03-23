4306a69219d084a98c460ac1df197317fb120ef8
Dan Shrout <danshrout@yahoo.com>
2020-03-22 19:15:09 -0700

    Updates

---
+++ precache-manifest.f2743ab926ce653d34d0611c60a9a03e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0dab062a2d25a0d9ec01aa1337cf431",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "55b8a3eaa779fe58f897",
+    "url": "/monsters/static/css/main.ce7eb085.chunk.css"
+  },
+  {
+    "revision": "0c1714d784b2321851fb",
+    "url": "/monsters/static/js/2.2fa5acd1.chunk.js"
+  },
+  {
+    "revision": "5493ac8c9e221eeba3bec532634c6faf",
+    "url": "/monsters/static/js/2.2fa5acd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55b8a3eaa779fe58f897",
+    "url": "/monsters/static/js/main.50ed8532.chunk.js"
+  },
+  {
+    "revision": "69ad86de3903771917ee",
+    "url": "/monsters/static/js/runtime-main.5e6993dc.js"
+  }
+]);
